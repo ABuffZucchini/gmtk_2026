3732b2b72942315dba7ef2cfa280c5dd9407fdae
ABuffZucchini <gagelannigan@gmail.com>
2026-07-23 16:47:01 -0400

    hoping i finally fixed it

--- scripts/Level.gd
+++ scripts/Level.gd
@@ -6,5 +6,5 @@ signal level_changed(level_name)
 func _on_button_pressed() -> void:
 	level_changed.emit(level_name)
 	
-	#get_tree().change_scene_to_file("res://Scenes/test_text.tscn")
+	#get_tree().change_scene_to_file("res://scenes/test_text.tscn")
 	pass # Replace with function body.
--- scripts/generic_spawn_point.gd
+++
@@ -1,6 +0,0 @@
-extends Sprite2D
-
-@export var clone:bool
-# hide in game, for spawn points and maybe other things later
-func _ready():
-  hide()

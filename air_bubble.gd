ff69b8cc0164dedc53b032a08db0d3d28400ec33
minidavid <njihiadavid02@gmail.com>
2026-07-25 05:49:58 +0300

    extra tweak

--- air_bubble.gd
+++ air_bubble.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 const gridSize = 16
 
 @onready var ray: RayCast2D = $CollisionShape2D/RayCast2D
+@export var extra_moves_added_when_urchin_touched_waterbubble: int = 3 
 
 func try_move_waterbubble(direction: Vector2):
 	ray.target_position = direction * gridSize
@@ -16,56 +17,10 @@ func try_move_waterbubble(direction: Vector2):
 		
 		if collider != null and collider.has_method("try_move_box") and  collider.try_move_box(direction):
 			#position += direction * gridSize
-			G.moves += 1
+			G.moves += extra_moves_added_when_urchin_touched_waterbubble
 			
 		return false
 		#
 	position+=direction*gridSize
 	return true
-	#
-
-
-#extends CharacterBody2D
-#
-#const GRID_SIZE = 16
-#const MOVE_SPEED = 12
-#
-#@onready var ray: RayCast2D = $CollisionShape2D/RayCast2D
-#
-#var target_position = Vector2.ZERO
-#var is_moving = false
-#
-#func _ready() -> void:
-	#target_position = position
-#
-#func _physics_process(delta: float) -> void:
-	#if is_moving:
-		#set_collision_layer_value(1,false)
-		#
-	#if position != target_position:
-		#position = position.lerp(target_position, MOVE_SPEED * delta)
-		#
-		#if position.distance_to(target_position) < 0.5:
-			#position = target_position
-			#is_moving = false
-			##set_collision_layer_value(1,true)
-				#
-#
-#
-#func try_move_box(direction: Vector2):
-	#if is_moving:
-		#return false
-	#
-	#ray.target_position = direction * GRID_SIZE
-	#ray.force_raycast_update()
-	#
-	#if ray.is_colliding():
-		#var collider = ray.get_collider()
-		#print(collider)
-		##return false
-		#
-	#target_position = position + (direction * GRID_SIZE)
-	#is_moving = true
-	#
-	##set_collision_layer_value(1,false)
-	#return true
+	
